d3868f539786bf05e466a55bc6720cbb3fc7c7ec
masonbot <62023052+masonbot@users.noreply.github.com>
2020-05-14 12:13:58 -0400

    Create hypotneuse.py

---
+++ hypotneuse.py
@@ -0,0 +1,7 @@
+from math import sqrt
+print("Insert opposite and adjacent sides of triangle:")
+a = float(input("a: "))
+b = float(input("b: "))
+
+c = sqrt(a**2 + b**2)
+print("The length of the hypotenuse is", c )
